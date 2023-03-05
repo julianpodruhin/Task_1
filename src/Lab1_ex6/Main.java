000c3bab56ee4ae47f7b94d1f3ced9f14c4cc530
yulianpodruhin <yulianpodruhin@mail.ru>
2023-03-05 16:01:48 +0300

    Lab_1ex_1-6

--- src/Lab1_ex6/Main.java
+++ src/Lab1_ex6/Main.java
@@ -1,7 +1,28 @@
 package Lab1_ex6;
 
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число");
+        int b = scanner.nextInt();
+
+        if (b % 2 != 0)
+        {
+            System.out.println("Нечетное " + b );
+        }
+        if (b % 2 == 0)
+        {
+            System.out.println("Четное " + b );
+        }
+        if (b % 2 == 0 & b>=100)
+        {
+            System.out.println("Выход за пределы диапазона " + b);
+        }
+
     }
 }
